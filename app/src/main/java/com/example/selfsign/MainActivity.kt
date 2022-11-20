7cf873a8d39a8dd1a8c148fc0eab1e8ed5ffd322
fyambos <feliciteyambos@gmail.com>
2022-11-20 22:29:44 +0100

    added firebase + update layout ScheduleActivity.kt

--- app/src/main/java/com/example/selfsign/MainActivity.kt
+++ app/src/main/java/com/example/selfsign/MainActivity.kt
@@ -5,18 +5,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // appel button avec id
+        // récuperer le bouton "login" crée dans le xml
         val loginButton: Button = findViewById(R.id.login)
 
-        // onclick appel
+        // ajouter une action au bouton lors du clic, dont l'intent est de rediriger vers une page "ScheduleActivity"
         loginButton.setOnClickListener{
-            startActivity(Intent(this, LoginActivity::class.java))
+            startActivity(Intent(this, ScheduleActivity::class.java))
             finish()
         }
     }
